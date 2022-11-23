2cdda6dc5daa060eafa93f0093deb83d7e0a46fc
Can Kupeli <cankupeli101@hotmail.com>
2022-11-23 10:24:14 +0100

    dagenslunch day-navbar now indicates what day is selected.

--- src/main/webapp/resources/js/lunch.js
+++ src/main/webapp/resources/js/lunch.js
@@ -9,12 +9,34 @@ document.addEventListener("DOMContentLoaded", function () {
     checkToday("Wednesday");
     checkToday("Thursday");
     checkToday("Friday");
+    document.getElementById("lunch-days-list-id").querySelectorAll('li').forEach(item => {
+        item.addEventListener('click', event => {
+            clickedDay(item.id);
+        })
+    })
+
 })
+
 
 function checkToday(date) {
     if (date == currentDay) {
         document.getElementById(date).innerHTML = "IDAG";
         document.getElementById(date).style.color = "#7D5A3E";
         document.getElementById(date).style.fontWeight = "bold";
+        document.getElementById(date).style.fontSize = "130%";
     }
+}
+
+function clickedDay(Day) {
+    document.getElementById("lunch-days-list-id").querySelectorAll('li').forEach(item => {
+        if (item.id == Day) {
+            document.getElementById(item.id).style.color = "#7D5A3E";
+            document.getElementById(item.id).style.fontWeight = "bold";
+            document.getElementById(item.id).style.fontSize = "130%";
+        } else {
+            document.getElementById(item.id).style.color = "black";
+            document.getElementById(item.id).style.fontWeight = "normal";
+            document.getElementById(item.id).style.fontSize = "100%";
+        }
+    })
 }
