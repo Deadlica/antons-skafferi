c5751013fd535c31ad0097543ed293317679f9bb
Eric Johansson <johanssonericwilliam@gmail.com>
2022-11-24 12:16:03 +0100

    changed var to let in JS

--- src/main/webapp/resources/js/admin.js
+++ src/main/webapp/resources/js/admin.js
@@ -1,19 +1,17 @@
-window.addEventListener('DOMContentLoaded', (event) => {
+window.addEventListener('DOMContentLoaded', () => {
     function getMondayOfCurrentWeek() {
         const today = new Date();
         const first = today.getDate() - today.getDay() + 1;
-
-        const monday = new Date(today.setDate(first));
-        return monday;
+        return new Date(today.setDate(first));
     }
 
     Date.prototype.getWeek = function () {
-        var date = new Date(this.getTime());
+        let date = new Date(this.getTime());
         date.setHours(0, 0, 0, 0);
         // Thursday in current week decides the year.
         date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
         // January 4 is always in week 1.
-        var week1 = new Date(date.getFullYear(), 0, 4);
+        let week1 = new Date(date.getFullYear(), 0, 4);
         // Adjust to Thursday in week 1 and count number of weeks from date to week1.
         return 1 + Math.round(((date.getTime() - week1.getTime()) / 86400000 - 3 + (week1.getDay() + 6) % 7) / 7);
     }
@@ -25,12 +23,12 @@ window.addEventListener('DOMContentLoaded', (event) => {
     }
 
     function setWeekDates() {
-        curr_date = getMondayOfCurrentWeek()
-        arr = document.querySelectorAll(".admin-day")
+        let curr_date = getMondayOfCurrentWeek()
+        let arr = document.querySelectorAll(".admin-day")
         for (let i = 0; i < 10; i++) {
             arr[i].firstElementChild.innerHTML = `${curr_date.getDate()} / ${curr_date.getMonth()}`
             curr_date.setDate(curr_date.getDate() + 1)
-            if (i == 4) {
+            if (i === 4) {
                 curr_date.setDate(curr_date.getDate() + 2)
             }
         }
