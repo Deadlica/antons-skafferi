a87e2292bb3c45f681a86bf5e532a482ef4f0010
MarcusJak <78412619+MarcusJak@users.noreply.github.com>
2022-12-07 10:35:47 +0100

    merge kalender in till master igen

---
+++ src/main/java/com/example/demo1/CalendarBean.java
@@ -0,0 +1,248 @@
+package com.example.demo1;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Named;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.net.ProxySelector;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+
+
+
+@Named(value = "CalendarBean")
+@ApplicationScoped
+public class CalendarBean implements Serializable {
+
+    private URL location = new URL();
+    private String link = location.getLink();
+
+
+    public class day {
+        int datum;
+        int id;
+        String type="null";
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getType() {
+            return type;
+        }
+
+        public void setType(String type) {
+            this.type = type;
+        }
+
+        public int getDatum() {
+            return datum;
+        }
+
+        public void setDatum(int datum) {
+            this.datum = datum;
+        }
+    }
+
+    BookingBean bookingBean=new BookingBean();
+
+    List<day> amountDays=new ArrayList<>();
+
+
+    public BookingBean getBookingBean() {
+        return bookingBean;
+    }
+
+    public void setBookingBean(BookingBean bookingBean) {
+        this.bookingBean = bookingBean;
+    }
+
+    BookingBean.infoBooking[] holdAllBookings;
+
+    private int shownMonth;
+    private int monthIndex =0;
+
+    private int shownYear;
+    private int startDayIndex;
+
+    private int thisDay;
+    private int thisMonth;
+    private int thisYear;
+    private int[] days_of_month={31,getFebDays(shownYear),31,30,31,30,31,31,30,31,30,31};
+
+    public CalendarBean() throws IOException, URISyntaxException, InterruptedException {
+
+        String[] localTimeHolder= String.valueOf(LocalDate.now()).split("-",3);
+        shownYear = Integer.parseInt(localTimeHolder[0]);
+        shownMonth = Integer.parseInt(localTimeHolder[1]);
+        thisYear= Integer.parseInt(localTimeHolder[0]);
+        thisMonth = Integer.parseInt(localTimeHolder[1]);
+        thisDay = Integer.parseInt(localTimeHolder[2]);
+
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        holdAllBookings= objectMapper.readValue(getJsonBookings(), BookingBean.infoBooking[].class);
+        startDayIndex = new Date(shownYear, shownMonth -1, 0).getDay();
+        generateCalendar();
+    }
+
+    public void nextMonth(){
+        if (monthIndex <2 ) {
+            monthIndex++;
+            shownMonth++;
+            if (shownMonth == 13) {
+                shownYear++;
+                shownMonth = 1;
+            }
+            startDayIndex = new Date(shownYear, shownMonth - 1, 0).getDay();
+
+            generateCalendar();
+        }
+    }
+    public void ConfirmAndSubmit() throws URISyntaxException, IOException, InterruptedException {
+        bookingBean.makeBooking();
+        ObjectMapper objectMapper = new ObjectMapper();
+        holdAllBookings= objectMapper.readValue(getJsonBookings(), BookingBean.infoBooking[].class);
+        generateCalendar();
+    }
+
+    public void prevMonth(){
+        if (monthIndex >0 ) {
+            monthIndex--;
+            shownMonth--;
+            if (shownMonth == 0) {
+                shownYear--;
+                shownMonth = 12;
+            }
+            startDayIndex = new Date(shownYear, shownMonth - 1, 0).getDay();
+
+            generateCalendar();
+        }
+    }
+
+    public int getShownYear() {
+        return shownYear;
+    }
+
+    public void setShownYear(int shownYear) {
+        this.shownYear = shownYear;
+    }
+    private boolean isLeapYear(int thisYear){
+        if ((thisYear % 400 == 0) || ((thisYear % 4 == 0) && (thisYear % 100 != 0))) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    private int getFebDays(int thisYear){
+
+        if (!isLeapYear(thisYear))
+            return 29;
+        else
+            return 28;
+    }
+
+    private void generateCalendar(){
+        if (startDayIndex ==0){
+            startDayIndex =7;
+        }
+        amountDays=new ArrayList<>();
+        int count=1;
+        for (int i=0;i<42;i++)
+        {
+
+            day temp=new day();
+            temp.id=i;
+            temp.datum=42+count;
+            if (startDayIndex -1<=i) {
+                temp.datum=count;
+                if (counting(shownYear,shownMonth,count)<1)
+                {
+                    if(thisDay>=count+1&&thisYear==shownYear&&thisMonth==shownMonth) {
+                        temp.type ="pastDay";
+                    }
+                    else {
+                        temp.type = "notFull";
+                    }
+                }
+                else {
+                    if(thisDay>=count+1&&thisYear==shownYear&&thisMonth==shownMonth) {
+                        temp.type ="pastDay";
+                    }
+                    else {
+                        temp.type = "full";
+                    }
+
+                }
+                count++;
+            }
+            amountDays.add(temp);
+
+            if (days_of_month[shownMonth -1]<count)
+            {
+                break;
+            }
+        }
+    }
+
+
+
+
+    public List<day> getAmountDays() {
+        return amountDays;
+    }
+
+    public void setAmountDays(List<day> amountDays) {
+        this.amountDays = amountDays;
+    }
+
+
+
+    public String getMonthName(){
+        String[] month_names= {"jan", "feb", "mar", "apr", "maj", "jun", "jul", "aug", "sep", "oct", "nov", "dec"};
+
+        return month_names[shownMonth -1];
+    }
+
+    private int counting(int yearCount, int monthCount, int dayCount)  {
+        int amount=0;
+        for (BookingBean.infoBooking var:holdAllBookings) {
+            String[] holder = String.valueOf(var.date).split("-", 3);
+
+            if(Integer.parseInt(holder[0]) == yearCount&&Integer.parseInt(holder[1]) == monthCount&&Integer.parseInt(holder[2]) == dayCount){
+                amount++;
+            }
+
+        }
+        return amount ;
+    }
+    private String getJsonBookings() throws URISyntaxException, IOException, InterruptedException  {
+        HttpRequest request2 = HttpRequest.newBuilder()
+                .uri(new URI("http://"+ this.link+":8080/antons-skafferi-db-1.0-SNAPSHOT/api/booking"))
+                .GET()
+                .build();
+
+        //10.82.231.15
+        HttpResponse<String> response = HttpClient
+                .newBuilder()
+                .proxy(ProxySelector.getDefault())
+                .build()
+                .send(request2, HttpResponse.BodyHandlers.ofString());
+        return response.body();
+    }
+
+}
--- src/main/webapp/resources/js/calendar.js
+++ src/main/webapp/resources/js/calendar.js
@@ -1,6 +1,5 @@
 isLeapYear=(year)=>{
     return (year%4 ===0&& year % 100 !== 0 && year %400 !== 0)||(year%100 === 0 && year% 400===0)
-
 }
 
 getFebDays=(year)=>{
@@ -13,44 +12,33 @@ getFebDays=(year)=>{
 
 const month_names=['jan','feb','mar','apr', 'maj','jun','jul','aug','sep','oct','nov','dec']
 
-let days=document.querySelector('.calender-days')
-let yearpicker=document.querySelector('.year-book')
-
 
 generateCalender=(month,year)=>{
     let currDate=new Date()
-    days.innerHTML='';
-    yearpicker.innerHTML=year
 
     let days_of_month =[31,getFebDays(year),31,30,31,30,31,31,30,31,30,31]
-    let month_picker=document.querySelector('.month')
-    month_picker.innerHTML="<p>"+month_names[month]+"</p>"
 
-    console.log(month_names[month])
     let first_day= new Date( year,month, 1)
-
+    let last_day= new Date( year,month+1, 1)
     firstDayStart=first_day.getDay()
 
     if(firstDayStart==0){
         firstDayStart=7
     }
-
-    for (let index =1; index<days_of_month[month]+firstDayStart;index++){
-        if (index>=firstDayStart){
-
-        if((index - firstDayStart+1)==currDate2&&curr_month==month&&curr_year==year){
-            days.innerHTML+= "<div id='"+(index- firstDayStart+1)+"' class ='selected week-day'>"+ (index - firstDayStart+1) +'</div>'
-        }
-        else if ((index - firstDayStart+1)==currDate.getDate()&&currDate.getMonth()==month&&currDate.getFullYear()==year){
-                days.innerHTML+= "<div id='"+(index- firstDayStart+1)+"' class ='this-day week-day'>"+ (index - firstDayStart+1) +'</div>'
-        }
-        else {
-            days.innerHTML+= "<div id='"+(index- firstDayStart+1)+"' class ='week-day'>"+ (index - firstDayStart+1)+"</div>"
-
-        }
+    console.log(curr_month+"----"+month)
+    for (let index =0; index<days_of_month[month]+firstDayStart;index++){
+        if (index>=firstDayStart-1){
+            if ((index - firstDayStart+2)==currDate.getDate()&&currDate.getMonth()==month&&currDate.getFullYear()==year)
+            {
+                document.getElementById("myform:j_idt19:"+(index)+":item2").classList.add("this-day")
+            }
+            if((index - firstDayStart+2)==currDate2 && curr_month==month+1&&currDate.getFullYear()==year) {
+                document.getElementById("myform:j_idt19:"+(index)+":item2").classList.add("selected")
+                prevTarget=document.getElementById("myform:j_idt19:"+(index)+":item2")
+            }
         }
         else {
-            days.innerHTML+= '<div></div>'
+            document.getElementById("myform:j_idt19:"+(index)+":item2").innerHTML="";
         }
     }
 
@@ -58,47 +46,42 @@ generateCalender=(month,year)=>{
 
 
 }
-
-
-document.querySelector('#prev-month').onclick=()=>{
-    month--
-    if (month<0) {
-        month=11
-        year= (year-1)
-        yearpicker.innerHTML=year
-    }
-    generateCalender(month, year)
-    loadNewDatesListners();
-}
-
-document.querySelector('#next-month').onclick=()=>{
-    month++
-
-
-if (month>11){
-    month=0
-    year= (year+1)
-        yearpicker.innerHTML=year
-
-
-    }
-    generateCalender(month, year)
-    loadNewDatesListners();
-}
+let prevTarget=undefined
 
 loadNewDatesListners=()=>{
-    let prevTarget=undefined
-    document.querySelectorAll('#days .week-day').forEach(day=>{
+    document.querySelectorAll('#days .notFull').forEach(day=>{
         day.addEventListener("click", async event => {
+            if (prevTarget!=undefined){
+                prevTarget.classList.remove("selected")
+            }
             event.currentTarget.classList.toggle("selected");
-
-            console.log(temp2)
+            prevTarget=event.currentTarget
             temp2.readOnly=false
-            temp2.value = year + "-" + (month + 1) + "-" + event.target.id
-            temp2.readOnly=true
 
-            document.getElementById('myform').submit();
-            console.log(document.getElementById('myform:inputText'))
+            var selectetMonth=(month_names.indexOf(month_picker.substring(0, 3)) + 1);
+            var selectetday=document.getElementById(event.target.id).innerHTML;
+
+            if (selectetMonth<10){
+                var tempSelectetMonth= "0"+String(selectetMonth);
+            }
+            else {
+                var tempSelectetMonth= String(selectetMonth);
+
+            }
+            if (selectetday.length==1){
+                var tempSelectetday= "0"+selectetday;
+            }
+            else {
+                var tempSelectetday= selectetday;
+
+            }
+            console.log(tempSelectetMonth+"!!!!!!!!"+tempSelectetday)
+
+            temp2.value = parseInt(yearpicker) + "-" + (tempSelectetMonth) + "-" + tempSelectetday
+            temp2.readOnly=true
+            console.log("------"+document.getElementById(event.target.id).innerHTML)
+
+            // document.getElementById('myform').submit();
 
 
         });
@@ -111,17 +94,19 @@ var temp2 = document.getElementById('myform:inputText')
 temp2.readOnly=true
 
 
-console.log(temp2.value)
 const myArray = temp2.value.split("-", 3);
 
-console.log(myArray)
+let yearpicker=document.querySelector('.year-book').innerHTML
+let month_picker=document.querySelector('.month').innerHTML
+console.log("---"+month_names.indexOf(month_picker.substring(0, 3)) +"-"+ parseInt(yearpicker))
+
+
+
+
 
 let currDate2=parseInt(myArray[2])
 let curr_month=parseInt(myArray[1])
 var curr_year=parseInt(myArray[0])
-curr_month=curr_month-1
-let year=curr_year
-let month=curr_month
 
-generateCalender(curr_month,curr_year)
+generateCalender(month_names.indexOf(month_picker.substring(0, 3)),parseInt(yearpicker))
 loadNewDatesListners();
