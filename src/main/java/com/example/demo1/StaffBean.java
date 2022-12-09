42440fb4dc1286c036f66e473d65a6d1d6bd9d30
Angelica Engström <angelica.princess@hotmail.com>
2022-12-09 16:21:14 +0100

    kan skapa en ny personal, converter försvann

--- src/main/java/com/example/demo1/StaffBean.java
+++ src/main/java/com/example/demo1/StaffBean.java
@@ -27,25 +27,9 @@ public class StaffBean implements Serializable {
     public void setSelectedDeletedEmployee(Employee selectedDeletedEmployee) {
         this.selectedDeletedEmployee = selectedDeletedEmployee;
     }
-
-    private String ssn;
-    private String firstName;
-    private String lastName;
-    private String email;
-    private String phoneNumber;
-
     private Employee selectedDeletedEmployee;
-    private Employee testEmp;
     private URL location = new URL();
     private String link = location.getLink();
-
-    public Employee getTestEmp() {
-        return testEmp;
-    }
-
-    public void setTestEmp(Employee testEmp) {
-        this.testEmp = testEmp;
-    }
 
     public StaffBean() throws IOException, URISyntaxException, InterruptedException {
         setJSONEmployees();
@@ -80,46 +64,6 @@ public class StaffBean implements Serializable {
 
     public void setEmployees(List<Employee> employees) {
         this.employees = employees;
-    }
-
-    public String getSsn() {
-        return ssn;
-    }
-
-    public void setSsn(String ssn) {
-        this.ssn = ssn;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
     }
 
     public HttpResponse<String> addStaff() throws URISyntaxException, IOException, InterruptedException {
