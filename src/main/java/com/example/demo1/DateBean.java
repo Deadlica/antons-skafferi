ba7280afe35c61a15fc96a72225187107bb6ddbd
Angelica Engström <angelica.princess@hotmail.com>
2022-12-17 00:27:57 +0100

    funktionalitet done

--- src/main/java/com/example/demo1/DateBean.java
+++ src/main/java/com/example/demo1/DateBean.java
@@ -2,6 +2,8 @@ package com.example.demo1;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.enterprise.context.RequestScoped;
+import jakarta.enterprise.context.SessionScoped;
+import jakarta.faces.view.ViewScoped;
 import jakarta.inject.Named;
 
 import java.io.IOException;
@@ -17,24 +19,31 @@ import java.time.LocalDate;
 import java.time.temporal.WeekFields;
 import java.util.*;
 
-@RequestScoped
+@SessionScoped
 @Named(value = "DateBean")
 public class DateBean implements Serializable {
-    private URL location = new URL();
-    private String link = location.getLink();
+    //private URL location = new URL();
+    //public String link = location.getLink();
 
     public String getDate() {
         return date;
     }
 
     public static class Weekday {
+        private URL location = new URL();
+        private String link = location.getLink();
         private final String text;
         private final String date;
         private String selected;
 
-        Weekday(String text, String date) {
+        private List<Employee> freeDinnerEmployees;
+        private List<Employee> freeLunchEmployees;
+
+        Weekday(String text, String date) throws URISyntaxException, IOException, InterruptedException {
             this.text = text;
             this.date = date;
+            freeDinnerEmployees = setFreeJSONEmployees(date, "dinner");
+            freeLunchEmployees = setFreeJSONEmployees(date, "lunch");
         }
 
         public String getDate() {
@@ -45,31 +54,72 @@ public class DateBean implements Serializable {
             return text;
         }
 
+        public List<Employee> getFreeDinnerEmployees() {
+            return freeDinnerEmployees;
+        }
+
+        public void setFreeDinnerEmployees(List<Employee> freeDinnerEmployees) {
+            this.freeDinnerEmployees = freeDinnerEmployees;
+        }
+
+        public List<Employee> getFreeLunchEmployees() {
+            return freeLunchEmployees;
+        }
+
+        public void setFreeLunchEmployees(List<Employee> freeLunchEmployees) {
+            this.freeLunchEmployees = freeLunchEmployees;
+        }
+
         public String getSelected() { return selected; }
         public void setSelected(String s) {
             this.selected = s;
+        }
+
+        public String getFreeJSONEmployees(String date, String url) throws IOException, InterruptedException, URISyntaxException {
+            HttpRequest request2 = HttpRequest.newBuilder()
+                    .uri(new URI("http://" + this.link + ":8080/antons-skafferi-db-1.0-SNAPSHOT/api/employee/" + url + "/available?date=" + date))
+                    .GET()
+                    .build();
+            HttpResponse<String> response = HttpClient
+                    .newBuilder()
+                    .proxy(ProxySelector.getDefault())
+                    .build()
+                    .send(request2, HttpResponse.BodyHandlers.ofString());
+            return response.body();
+        }
+
+        public List<Employee> setFreeJSONEmployees(String date, String url) throws IOException, URISyntaxException, InterruptedException {
+            List<Employee> freeEmployees = new ArrayList<>();
+            ObjectMapper objectMapper = new ObjectMapper();
+            Employee[] list_arr = objectMapper.readValue(getFreeJSONEmployees(date, url), Employee[].class);
+            List<Employee> arr = new ArrayList<>(Arrays.asList(list_arr));
+
+            freeEmployees.addAll(arr);
+
+            return freeEmployees;
         }
     }
 
     DateBean() throws IOException, URISyntaxException, InterruptedException {
         week = getCurrWeek();
+        setLunchShift();
+        setDinnerShift();
+        /*
         weekdays.add(new Weekday(getText(Calendar.MONDAY), getDay(Calendar.MONDAY)));
-        //freeMondayNightEmployees = setFreeJSONEmployees(getDay(Calendar.WEDNESDAY), "dinner");
-        //mondayNightSelected = freeMondayNightEmployees.get(0);
-
         weekdays.add(new Weekday(getText(Calendar.TUESDAY), getDay(Calendar.TUESDAY)));
-
         weekdays.add(new Weekday(getText(Calendar.WEDNESDAY), getDay(Calendar.WEDNESDAY)));
         weekdays.add(new Weekday(getText(Calendar.THURSDAY), getDay(Calendar.THURSDAY)));
         weekdays.add(new Weekday(getText(Calendar.FRIDAY), getDay(Calendar.FRIDAY)));
         weekdays.add(new Weekday(getText(Calendar.SATURDAY), getDay(Calendar.SATURDAY)));
-        weekdays.add(new Weekday(getText(Calendar.SUNDAY), getDay(Calendar.SUNDAY)));
+        weekdays.add(new Weekday(getText(Calendar.SUNDAY), getDay(Calendar.SUNDAY)));*/
 
+        /*
         earlyweekdays.add(new Weekday(getText(Calendar.MONDAY), getDay(Calendar.MONDAY)));
         earlyweekdays.add(new Weekday(getText(Calendar.TUESDAY), getDay(Calendar.TUESDAY)));
         earlyweekdays.add(new Weekday(getText(Calendar.WEDNESDAY), getDay(Calendar.WEDNESDAY)));
         earlyweekdays.add(new Weekday(getText(Calendar.THURSDAY), getDay(Calendar.THURSDAY)));
         earlyweekdays.add(new Weekday(getText(Calendar.FRIDAY), getDay(Calendar.FRIDAY)));
+         */
     }
 
     public void setDate(String date) {
@@ -80,20 +130,42 @@ public class DateBean implements Serializable {
         return new Date().toString();
     }
 
-    public void decreaseWeek() {
-        week = week - 1;
-        if (week < 0) {
-            week = week + 52;
+    public void decreaseWeek() throws URISyntaxException, IOException, InterruptedException {
+        this.week -= 1;
+        if (this.week < 1) {
+            this.week += 52;
         }
-        setWeek(week);
+        setLunchShift();
+        setDinnerShift();
     }
 
-    public void increaseWeek(int week) {
-        this.week = week + 1;
+    public void increaseWeek() throws URISyntaxException, IOException, InterruptedException {
+        this.week += 1;
         if (this.week > 52) {
-            this.week = week - 52;
+            this.week -= 52;
         }
-        //setWeek(this.week);
+        setLunchShift();
+        setDinnerShift();
+    }
+
+    public void setDinnerShift() throws URISyntaxException, IOException, InterruptedException {
+        weekdays.clear();
+        weekdays.add(new Weekday(getText(Calendar.MONDAY), getDay(Calendar.MONDAY)));
+        weekdays.add(new Weekday(getText(Calendar.TUESDAY), getDay(Calendar.TUESDAY)));
+        weekdays.add(new Weekday(getText(Calendar.WEDNESDAY), getDay(Calendar.WEDNESDAY)));
+        weekdays.add(new Weekday(getText(Calendar.THURSDAY), getDay(Calendar.THURSDAY)));
+        weekdays.add(new Weekday(getText(Calendar.FRIDAY), getDay(Calendar.FRIDAY)));
+        weekdays.add(new Weekday(getText(Calendar.SATURDAY), getDay(Calendar.SATURDAY)));
+        weekdays.add(new Weekday(getText(Calendar.SUNDAY), getDay(Calendar.SUNDAY)));
+    }
+
+    public void setLunchShift() throws URISyntaxException, IOException, InterruptedException {
+        earlyweekdays.clear();
+        earlyweekdays.add(new Weekday(getText(Calendar.MONDAY), getDay(Calendar.MONDAY)));
+        earlyweekdays.add(new Weekday(getText(Calendar.TUESDAY), getDay(Calendar.TUESDAY)));
+        earlyweekdays.add(new Weekday(getText(Calendar.WEDNESDAY), getDay(Calendar.WEDNESDAY)));
+        earlyweekdays.add(new Weekday(getText(Calendar.THURSDAY), getDay(Calendar.THURSDAY)));
+        earlyweekdays.add(new Weekday(getText(Calendar.FRIDAY), getDay(Calendar.FRIDAY)));
     }
 
     public int getCurrWeek() {
@@ -104,6 +176,7 @@ public class DateBean implements Serializable {
 
     public String getText(int day) {
         Calendar c = Calendar.getInstance();
+        c.set(Calendar.WEEK_OF_YEAR, week);
         c.set(Calendar.DAY_OF_WEEK, day);
         Date date = c.getTime();
         SimpleDateFormat dateFormat = new SimpleDateFormat("EEE d/M");
@@ -112,6 +185,7 @@ public class DateBean implements Serializable {
 
     public String getDay(int day) {
         Calendar c = Calendar.getInstance();
+        c.set(Calendar.WEEK_OF_YEAR, week);
         c.set(Calendar.DAY_OF_WEEK, day);
         Date date = c.getTime();
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
@@ -146,30 +220,4 @@ public class DateBean implements Serializable {
     public void setEarlyweekdays(List<Weekday> earlyweekdays) {
         this.earlyweekdays = earlyweekdays;
     }
-
-/*
-    public String getFreeJSONEmployees(String date, String url) throws IOException, InterruptedException, URISyntaxException {
-        HttpRequest request2 = HttpRequest.newBuilder()
-                .uri(new URI("http://" + this.link + ":8080/antons-skafferi-db-1.0-SNAPSHOT/api/employee/" + url + "/available?date=" + date))
-                .GET()
-                .build();
-        HttpResponse<String> response = HttpClient
-                .newBuilder()
-                .proxy(ProxySelector.getDefault())
-                .build()
-                .send(request2, HttpResponse.BodyHandlers.ofString());
-        return response.body();
-    }
-
-    public List<Employee> setFreeJSONEmployees(String date, String url) throws IOException, URISyntaxException, InterruptedException {
-        //freeEmployees.clear();
-        List<Employee> freeEmployees = new ArrayList<>();
-        ObjectMapper objectMapper = new ObjectMapper();
-        Employee[] list_arr = objectMapper.readValue(getFreeJSONEmployees(date, url), Employee[].class);
-        List<Employee> arr = new ArrayList<>(Arrays.asList(list_arr));
-
-        freeEmployees.addAll(arr);
-        //setSelectedEmployee(date, url);
-        return freeEmployees;
-    }*/
 }
