9a845c98244a72a43bf162c869d6f85ddb070535
Angelica Engström <angelica.princess@hotmail.com>
2023-01-10 15:46:19 +0100

    adminfunktionalitet klar

---
+++ src/main/java/com/example/demo1/Shift.java
@@ -0,0 +1,49 @@
+package com.example.demo1;
+
+public class Shift {
+    private int id;
+    private String date;
+    private String beginTime;
+    private String endTime;
+    private Employee employee;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getBeginTime() {
+        return beginTime;
+    }
+
+    public void setBeginTime(String beginTime) {
+        this.beginTime = beginTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+}
